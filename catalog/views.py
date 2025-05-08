eb188fb351d648782f8e7cb28d24ffe51b1d22f2
Maksim Charin <bonfiglioly@gmail.com>
2025-05-08 03:16:58 +0300

    add contacts.html

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 def home(request):
     return render(request, 'home.html')
+
+
+def contacts(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+
+        return HttpResponse(f"{name}, Ваши данные отправлены!")
+    return render(request, 'contacts.html')
