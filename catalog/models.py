d168bae1257bb7c2b6456004f9101a898b780e96
Maksim Charin <bonfiglioly@gmail.com>
2025-05-13 14:38:57 +0300

    add admin, upd. models

--- catalog/models.py
+++ catalog/models.py
@@ -5,17 +5,29 @@ class Category(models.Model):
     description = models.TextField(null=True, blank=True, verbose_name='описание')
 
 
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ['name',]
+
+
+
+
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='наименование')
     description = models.TextField(null=True, blank=True, verbose_name='описание')
-    image = models.ImageField(upload_to='photos/', verbose_name='изображение')
+    image = models.ImageField(upload_to='photos/', null=True, blank=True, verbose_name='изображение')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
-    price = models.FloatField(verbose_name='цена')
+    price = models.FloatField(null=True, blank=True, verbose_name='цена')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return {self.name}
+        return f"{self.name} цена: {self.price} {self.created_at} "
 
 
     class Meta:
