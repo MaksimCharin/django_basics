8f737a60bb78bedec4bad85a67cf6baa967e5f19
Maksim Charin <bonfiglioly@gmail.com>
2025-05-13 11:27:17 +0300

    update db info, add .env.sample, add models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.TextField(null=True, blank=True, verbose_name='описание')
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.TextField(null=True, blank=True, verbose_name='описание')
+    image = models.ImageField(upload_to='photos/', verbose_name='изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+    price = models.FloatField(verbose_name='цена')
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return {self.name}
+
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ['name', 'price', 'category']
+
+
+
