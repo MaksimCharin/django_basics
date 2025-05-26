d0ddc77be15af6af4e9e2c18ca6de41f1471310e
Maksim Charin <bonfiglioly@gmail.com>
2025-05-26 20:41:57 +0300

    add success url redirect and filter

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,16 @@
 from blog.models import BlogMessage
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 
 
 class MessageListView(ListView):
     model = BlogMessage
     template_name = 'blog/message_list.html'
     context_object_name = 'messages'
+
+    def get_queryset(self):
+        return BlogMessage.objects.filter(is_published=True)
 
 
 class MessageDetailView(DetailView):
@@ -34,6 +37,9 @@ class MessageUpdateView(UpdateView):
     fields = ['title', 'description', 'preview']
     template_name = 'blog/message_form.html'
     success_url = reverse_lazy('blog:message_list')
+
+    def get_success_url(self):
+        return reverse('blog:message_detail', args=[self.kwargs.get('pk')])
 
 
 class MessageDeleteView(DeleteView):
